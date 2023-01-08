e179dd3bf1239890d950b155b0a1f578794c70c5
Fábio Augusto <fabio.augusto1911@gmail.com>
2023-01-08 18:27:09 -0300

    - Add typescript. - dev environment setup.

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,10 @@
-console.log("Executei");
+import express, { Request, Response, NextFunction } from 'express';
+
+const app = express();
+const port = 44390;
+
+app.get('/status', (req: Request, res: Response, next: NextFunction) => {
+    res.status(200).send({ foo: 'bar' });
+});
+
+app.listen(port, () => console.log(`Aplicação rodando na porta ${port}!`));
